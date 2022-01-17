09b579ea9bd4e381d8f61a202c43f9c2199cce1b
kylebennison <kylebeni012@gmail.com>
2022-01-17 11:14:10 -0500

    more fun details

--- init_git.py
+++ init_git.py
@@ -3,3 +3,23 @@
 print('Hello Python Learner')
 
 # This file serves no purpose other than to ensure git is working.
+
+'''
+import os
+
+os.getcwd() # gets your working directory
+os.chdir() # changes your directory. remember on windows to use \\ (double backslashes)
+
+!git status # get status in the python console
+
+!git add filename.py
+
+!git commit filename.py -m 'your message here'
+
+!git pull
+
+!git push
+
+Simple!
+
+'''
